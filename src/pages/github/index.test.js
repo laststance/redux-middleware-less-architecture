9f6fa2db9f5875c014689f7be5481c22779deb57
ryota-murakami <dojce1048@gmail.com>
2018-05-11 20:25:45 +0900

    tmp

---
+++ src/pages/github/index.test.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import { render, Simulate, wait } from 'react-testing-library'
+import 'dom-testing-library/extend-expect'
+import axiosMock from 'axios'
+import { Github } from './index'
+
+test('foo', async () => {})
