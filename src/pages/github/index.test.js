70b7fed7440bc380b9819e431cffe0ef9bb87ba3
ryota-murakami <dojce1048@gmail.com>
2018-05-14 02:44:37 +0900

    npm: bump

--- src/pages/github/index.test.js
+++ src/pages/github/index.test.js
@@ -3,5 +3,11 @@ import { render, Simulate, wait } from 'react-testing-library'
 import 'dom-testing-library/extend-expect'
 import axiosMock from 'axios'
 import { Github } from './index'
+import mockResponse from './mockResponse'
 
-test('foo', async () => {})
+test('foo', async () => {
+  const initialState = {}
+  const dispatch = f => f
+  axiosMock.get.mockResolvedValueOnce({ data: mockResponse })
+  const { getByText, getByTestId, container } = render(<Github />)
+})
