bfd85abb867b827623e3bd738b5fee4cf52b8d31
ryota-murakami <dojce1048@gmail.com>
2018-07-15 06:46:00 +0900

    dev message

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -2,16 +2,17 @@
 import React from 'react'
 import { Container, Head, Github } from './style'
 
-// TODO Error Bound
+// TODO Error Boundaly
 const index = () => {
   return (
     <Container>
       <Head>
         <h1>Redux Zero Middleware</h1>
       </Head>
-      <Github to="/github">GitHub Repo Search Example</Github>
+      <Github to="/github">
+        GitHub Repo Search Example(still development🙏)
+      </Github>
     </Container>
   )
 }
-
 export default index
