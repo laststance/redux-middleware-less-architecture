70efdb52ee186e34efe70aa3634dfd8d0cead1c3
ryota-murakami <dojce1048@gmail.com>
2018-08-28 21:46:47 +0900

    react-loadable

--- README.md
+++ README.md
@@ -4,7 +4,6 @@
 [![PRs Welcome](https://img.shields.io/badge/PRs-welcome-brightgreen.svg?style=flat-square)](http://makeapullrequest.com)
 [![tested with jest](https://img.shields.io/badge/tested_with-jest-99424f.svg)](https://github.com/facebook/jest)
 [![code style: prettier](https://img.shields.io/badge/code_style-prettier-ff69b4.svg)](https://github.com/prettier/prettier)
-[![codecov](https://codecov.io/gh/ryota-murakami/redux-no-middleware-pattarn/branch/master/graph/badge.svg)](https://codecov.io/gh/ryota-murakami/redux-boilerplate-less-architecture)
 [![All Contributors](https://img.shields.io/badge/all_contributors-1-orange.svg?style=flat-square)](#contributors)
 
 > 🍷 write redux application from boilerplate less.
--- src/index.js
+++ src/index.js
@@ -1,12 +1,40 @@
 import React, { Fragment } from 'react'
 import ReactDOM from 'react-dom'
+import styled from 'styled-components'
+import Loadable from 'react-loadable'
+import { CircleLoader } from 'react-spinners'
 import { createStore } from 'redux'
 import reducer from './reducer'
 import { Provider } from 'react-redux'
 import { BrowserRouter, Route } from 'react-router-dom'
-import IndexPage from './pages/index'
-import GithubPage from './pages/github'
 import './index.css'
+
+const Container = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
+const Loading = () => {
+  return (
+    <Container>
+      <CircleLoader sizeUnit={'px'} size={150} color={'#3BD6B7'} />
+    </Container>
+  )
+}
+
+/* prettier-ignore */
+const IndexPage = Loadable({
+  loader: () => import('./pages/index'/* webpackChunkName: "IndexPage" */),
+  loading: Loading
+})
+/* prettier-ignore */
+const GithubPage = Loadable({
+  loader: () => import('./pages/github'/* webpackChunkName: "GithubPage" */),
+  loading: Loading
+})
 
 const store = createStore(
   reducer,
